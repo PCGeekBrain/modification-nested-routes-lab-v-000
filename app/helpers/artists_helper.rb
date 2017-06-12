a2f62c7f2a484d28a3d5124f07d137741345e2a6
Menachem Hornbacher <me@mendel.tech>
2017-06-12 14:26:34 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, nested_route)
+    if nested_route
+      text_field_tag("song[artist_name]", !song.artist.nil? ? song.artist.name : "", id: "song_artist_name")
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name), id: "song_artist_id"
+    end
+  end
 end
